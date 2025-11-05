525d64fff865d28b09aa5ee5582a82a73d80e694
Pedro Salomon <pedro.salomon@gmail.com>
2025-11-05 12:07:18 +0100

    added  testingh environment

--- backend/project-04/src/main/java/Main.java
+++ backend/project-04/src/main/java/Main.java
@@ -1,15 +1,35 @@
+import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 
 @SpringBootApplication
 @ComponentScan({"controllers", "repositories", "services"})
 @EntityScan("entities")
 @EnableJpaRepositories({"repositories"})
 public class Main {
+    @SneakyThrows
     public static void main(String[] args) {
+        var envs = System.getenv();
+
+        envs.forEach( (k, v)-> System.out.println("Key : "+k + " Values: "+  v ) );
+
+        File file = ResourceUtils.getFile(".env");
+
+        System.out.println(file.getAbsoluteFile());
+
+        var content =Files.readString(file.toPath(), Charset.defaultCharset());
+
+        System.out.println(content);
+
         SpringApplication.run(Main.class, args);
     }
 }

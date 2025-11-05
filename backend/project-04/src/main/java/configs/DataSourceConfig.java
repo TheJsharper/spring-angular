5ee1d954e0a4d52a38e841d788c39b9662a4d48c
Pedro Salomon <pedro.salomon@gmail.com>
2025-11-05 21:01:22 +0100

    added profiles prod and test

---
+++ backend/project-04/src/main/java/configs/DataSourceConfig.java
@@ -0,0 +1,25 @@
+package configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+@Configuration
+public class DataSourceConfig {
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driverClassName"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+        System.out.println("======>"+ dataSource.getUrl()+ dataSource.getUsername());
+        return dataSource;
+    }
+}
---
+++ backend/project-04/src/test/java/unit/DevActiveProfileUnitTest.java
@@ -0,0 +1,36 @@
+package unit;
+
+
+import configs.DataSourceConfig;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(classes = DataSourceConfig.class)
+@ActiveProfiles("test")
+public class DevActiveProfileUnitTest {
+    @Value("${app.info}")
+    private String propertyString;
+
+    @Autowired
+    private Environment env;
+
+    @Test
+    void whenDevIsActive_thenValueShouldBeKeptFromDedicatedApplicationFile() {
+        String currentProfile = env.getActiveProfiles()[0];
+
+        Assertions.assertEquals(String.format("This is the TEST Environment property file", currentProfile), propertyString);
+    }
+    @Test
+    void whenDevIsActive_thenValueShouldBeKeptFromDedicatedApplicationGetProperty() {
+        String currentProfile = env.getActiveProfiles()[0];
+
+        var message  =env.getProperty("app.info");
+
+        Assertions.assertEquals(String.format("This is the TEST Environment property file", currentProfile), message);
+    }
+}

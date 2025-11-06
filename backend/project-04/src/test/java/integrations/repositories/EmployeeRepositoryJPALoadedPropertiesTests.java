5465653cf696d39d39d987e1c847d09907a468fd
Pedro Salomon <pedro.salomon@gmail.com>
2025-11-06 22:45:58 +0100

    crud repository tested

--- backend/project-04/src/test/java/integrations/repositories/EmployeeRepositoryJPALoadedPropertiesTests.java
+++ backend/project-04/src/test/java/integrations/repositories/EmployeeRepositoryJPALoadedPropertiesTests.java
@@ -12,6 +12,14 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import repositories.EmployeeRepositories;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.StreamSupport;
+
 
 @DataJpaTest(
         excludeAutoConfiguration = {DataSourceAutoConfiguration.class}
@@ -64,9 +72,10 @@ public class EmployeeRepositoryJPALoadedPropertiesTests {
 
 
         foundEmployee.setFirstName("!Test update firstName");
+
         foundEmployee.setLastName("!Test update lasName");
 
-       var updateEmployee = employeeRepositories.save(foundEmployee);
+        var updateEmployee = employeeRepositories.save(foundEmployee);
 
 
         Assertions.assertEquals(foundEmployee.getId(), updateEmployee.getId());
@@ -78,4 +87,94 @@ public class EmployeeRepositoryJPALoadedPropertiesTests {
         Assertions.assertEquals(foundEmployee, updateEmployee);
 
     }
+
+    @Test
+    public void testFoundByIdEmployee() {
+        Employee employee = new Employee("Test firstName", "Test lastName");
+
+        var saveEmployee = employeeRepositories.save(employee);
+
+        var foundEmployee = employeeRepositories.findById(employee.getId()).orElseThrow();
+
+        Assertions.assertEquals(foundEmployee.getId(), saveEmployee.getId());
+
+        Assertions.assertEquals(foundEmployee.getFirstName(), saveEmployee.getFirstName());
+
+        Assertions.assertEquals(foundEmployee.getLastName(), saveEmployee.getLastName());
+
+        Assertions.assertEquals(foundEmployee, saveEmployee);
+
+    }
+
+    @Test
+    public void testDeleteByIdEmployee() {
+        Employee employee = new Employee("Test firstName", "Test lastName");
+
+        var saveEmployee = employeeRepositories.save(employee);
+
+        employeeRepositories.deleteById(saveEmployee.getId());
+
+        var exception = Assertions.assertThrows(NoSuchElementException.class, () -> employeeRepositories.findById(employee.getId()).orElseThrow());
+
+        String expectedMessage = "No value present";
+
+        String actualMessage = exception.getMessage();
+
+        Assertions.assertEquals(expectedMessage, actualMessage);
+
+
+    }
+
+    @Test
+    public void testDeleteEmployee() {
+        Employee employee = new Employee("Test firstName", "Test lastName");
+
+        var saveEmployee = employeeRepositories.save(employee);
+
+        employeeRepositories.delete(saveEmployee);
+
+        var exception = Assertions.assertThrows(NoSuchElementException.class, () -> employeeRepositories.findById(employee.getId()).orElseThrow());
+
+        String expectedMessage = "No value present";
+
+        String actualMessage = exception.getMessage();
+
+        Assertions.assertEquals(expectedMessage, actualMessage);
+
+
+    }
+
+
+    @Test
+    public void testFindAllEmployee() {
+
+        var all = IntStream.range(0, 12).mapToObj((next) -> new Employee("Test firstName :" + next, "Test lastName:" + next)).toList();
+
+        var allSavedEmployeeIterable = employeeRepositories.saveAll(all);
+
+        var ids = StreamSupport.stream(allSavedEmployeeIterable.spliterator(), false).map(Employee::getId).collect(Collectors.toList());
+
+        var foundIds = employeeRepositories.findAllById(ids);
+
+        Assertions.assertArrayEquals(ids.toArray(), StreamSupport.stream(foundIds.spliterator(), false).map(Employee::getId).toList().toArray());
+
+
+    }
+
+    @Test
+    public void testSavedAllFindAllEmployee() {
+
+        var all = IntStream.range(0, 12).mapToObj((next) -> new Employee("Test firstName :" + next, "Test lastName:" + next)).toList();
+
+        var allSavedEmployeeIterable = employeeRepositories.saveAll(all);
+
+        var ids = StreamSupport.stream(allSavedEmployeeIterable.spliterator(), false).map(Employee::getId).collect(Collectors.toList());
+
+        var foundIds = employeeRepositories.findAllById(ids);
+
+        Assertions.assertArrayEquals(ids.toArray(), StreamSupport.stream(foundIds.spliterator(), false).map(Employee::getId).toList().toArray());
+
+
+    }
+
 }

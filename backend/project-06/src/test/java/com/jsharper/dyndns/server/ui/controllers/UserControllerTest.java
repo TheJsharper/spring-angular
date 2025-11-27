02f129e1135e3729e283e2d311c8e298c8f8f943
Pedro Salomon <pedro.salomon@gmail.com>
2025-11-27 23:08:14 +0100

    improved testing

--- backend/project-06/src/test/java/com/jsharper/dyndns/server/ui/controllers/UserControllerTest.java
+++ backend/project-06/src/test/java/com/jsharper/dyndns/server/ui/controllers/UserControllerTest.java
@@ -6,6 +6,7 @@ import com.jsharper.dyndns.server.shared.UserDto;
 import com.jsharper.dyndns.server.ui.request.UserDetailsRequestModel;
 import com.jsharper.dyndns.server.ui.response.UserRest;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -13,6 +14,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -30,22 +32,29 @@ public class UserControllerTest {
     @MockitoBean
     private UsersService usersService;
 
-    @Test
-    @DisplayName("User can be created")
-    void testCreateUser_whenValidUserDetailsProvided_returnsCreateUserDetails() throws Exception {
-        //Arrange
-        UserDetailsRequestModel userDetailsRequestModel = new UserDetailsRequestModel();
+    private UserDetailsRequestModel userDetailsRequestModel;
+
+    @BeforeEach
+    public void init() {
+        userDetailsRequestModel = new UserDetailsRequestModel();
         userDetailsRequestModel.setFirstName("joe");
         userDetailsRequestModel.setLastName("Burry");
         userDetailsRequestModel.setEmail("email@test.com");
         userDetailsRequestModel.setPassword("12345678");
         userDetailsRequestModel.setRepeatPassword("12345678");
 
-       /* UserDto userDto = new UserDto();
-        userDto.setEmail("joe");
-        userDto.setLastName("Burry");
-        userDto.setEmail("email");
-        userDto.setUserId(UUID.randomUUID().toString());*/
+    }
+
+    @Test
+    @DisplayName("User can be created")
+    void testCreateUser_whenValidUserDetailsProvided_returnsCreateUserDetails() throws Exception {
+        //Arrange
+        userDetailsRequestModel.setFirstName("joe");
+        userDetailsRequestModel.setLastName("Burry");
+        userDetailsRequestModel.setEmail("email@test.com");
+        userDetailsRequestModel.setPassword("12345678");
+        userDetailsRequestModel.setRepeatPassword("12345678");
+
 
         UserDto userDto = new ModelMapper().map(userDetailsRequestModel, UserDto.class);
         userDto.setUserId(UUID.randomUUID().toString());
@@ -61,13 +70,47 @@ public class UserControllerTest {
         //Act
         var result = mockMvc.perform(requestBuilder).andReturn();
         var responseBodyAsString = result.getResponse().getContentAsString();
-        System.out.println(responseBodyAsString);
         var userResponse = new ObjectMapper().readValue(responseBodyAsString, UserRest.class);
-        //Assert
 
+        //Assert
         Assertions.assertEquals(userDetailsRequestModel.getFirstName(), userResponse.getFirstName());
         Assertions.assertEquals(userDetailsRequestModel.getLastName(), userResponse.getLastName());
         Assertions.assertEquals(userDetailsRequestModel.getEmail(), userResponse.getEmail());
-        //Assertions.assertEquals(userDetailsRequestModel.ge,userResponse.get  );
+
+        Assertions.assertFalse(userResponse.getUserId().isEmpty());
+    }
+
+    @Test
+    @DisplayName("First Name is not empty")
+    void testCreateUser_whenFirstNameIsNotProvided_returns400StatusCode() throws Exception {
+        //Arrange
+        userDetailsRequestModel.setFirstName("");
+        var requestBuilder = MockMvcRequestBuilders.post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(userDetailsRequestModel));
+
+        //Act
+        var result = mockMvc.perform(requestBuilder).andReturn();
+
+        //Assert
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), result.getResponse().getStatus());
+    }
+
+    @Test
+    @DisplayName("First Name is only 1 character it must be leat at 2")
+    void testCreateUser_whenFirstNameIsOnlyOneCharacter_returns400StatusCode() throws Exception {
+        //Arrange
+        userDetailsRequestModel.setFirstName("A");
+        var requestBuilder = MockMvcRequestBuilders.post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(userDetailsRequestModel));
+
+        //Act
+        var result = mockMvc.perform(requestBuilder).andReturn();
+
+        //Assert
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), result.getResponse().getStatus());
     }
 }

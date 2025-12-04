df61689cb7ccb1bf09deb2ad319d2bec7632e81e
Pedro Salomon <pedro.salomon@gmail.com>
2025-12-04 14:06:23 +0100

    ordered test cases

--- backend/project-07/src/test/java/com/jsharper/dyndns/server/repositories/integrations/ProductRepositoriesTest.java
+++ backend/project-07/src/test/java/com/jsharper/dyndns/server/repositories/integrations/ProductRepositoriesTest.java
@@ -10,6 +10,7 @@ import org.springframework.data.util.StreamUtils;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Random;
+import java.util.function.Supplier;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
@@ -28,56 +29,22 @@ public class ProductRepositoriesTest {
 
     private ProductEntity storedProduct;
 
-    @Test
-    @Order(1)
-    @DisplayName("Create Product and test return object with it")
-    void createProduct_whenValidObject_returnsStoredObject() {
-        //Arrange
-        var product = new ProductEntity("Test", "test desc", 15.5);
+    private Supplier<Stream<ProductEntity>> storeAllProductEntity;
 
-        //Act
-        var storeProduct = productRepository.save(product);
-        this.storedProduct = storeProduct;
-
-        //Assert
-        assertEquals(product.getName(), storeProduct.getName());
-        assertEquals(product.getDesc(), storeProduct.getDesc());
-        assertEquals(product.getPrice(), storeProduct.getPrice());
-        assertNotNull(storeProduct.getId());
-    }
-
-    @Test
-    @Order(2)
-    @DisplayName("Find by Product id and test with stored Product")
-    void createProduct_whenValidObject_returnsFindById() {
-        //Arrange
-
-        //Act
-        var storedProduct = productRepository.findById(this.storedProduct.getId()).orElseThrow();
-
-        //Assert
-        assertNotNull(storedProduct.getName());
-        assertNotNull(storedProduct.getDesc());
-        assertNotNull(storedProduct.getPrice());
-        assertNotNull(storedProduct.getId());
-
-        assertEquals(storedProduct.getId(), this.storedProduct.getId());
-        assertEquals(storedProduct.getName(), this.storedProduct.getName());
-        assertEquals(storedProduct.getDesc(), this.storedProduct.getDesc());
-        assertEquals(storedProduct.getPrice(), this.storedProduct.getPrice());
-    }
 
     @TestFactory
-    @Order(3)
+    @Order(1)
     @DisplayName("create with saveAll(iterable) returns stored list ")
     Stream<DynamicTest> testIterateProduct_whenGetsArguments_returnsDynamicTest() {
         Iterable<ProductEntity> iterator = getArguments().toList();
 
-        var storedIterator = this.productRepository.saveAll(iterator).stream();
+        Supplier<Stream<ProductEntity>> storedIterator = () -> this.productRepository.saveAll(iterator).stream();
+
+        this.storeAllProductEntity = storedIterator;
 
         var stream = StreamSupport.stream(iterator.spliterator(), false);
 
-        var pairs = StreamUtils.zip(storedIterator, stream, Pair::of);
+        var pairs = StreamUtils.zip(storedIterator.get(), stream, Pair::of);
 
         var breakLine = System.lineSeparator();
 
@@ -104,6 +71,71 @@ public class ProductRepositoriesTest {
                 .iterate(0, (next) -> next + 2)
                 .limit(100)
                 .mapToObj(next -> new ProductEntity("Product" + next, "Product Description " + next, random.nextDouble(10.0, 500.00)));
+    }
+
+    @TestFactory
+    @Order(2)
+    @DisplayName("Find all stored entities in db")
+    Stream<DynamicTest> testFindAll_whenCallFindAllWithParameters_returnAllExistedEntitiesInDB() {
+        var stream = productRepository.findAll().stream();
+
+        var pairs = StreamUtils.zip(this.storeAllProductEntity.get(), stream, Pair::of);
+
+        var breakLine = System.lineSeparator();
+
+        return pairs.map(p ->
+                DynamicTest.dynamicTest(String.format("First Name %s second Name %s" + breakLine
+                                        + " First description %s Second Description %s" + breakLine
+                                        + "First price %f Second price %f",
+                                p.getFirst().getName(), p.getSecond().getName(),
+                                p.getFirst().getDesc(), p.getSecond().getDesc(),
+                                p.getFirst().getPrice(), p.getSecond().getPrice()
+                        ),
+                        () -> {
+                            assertEquals(p.getFirst().getName(), p.getSecond().getName());
+                            assertEquals(p.getFirst().getDesc(), p.getSecond().getDesc());
+                            assertEquals(p.getFirst().getPrice(), p.getSecond().getPrice());
+                        }
+                ));
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Create Product and test return object with it")
+    void createProduct_whenValidObject_returnsStoredObject() {
+        //Arrange
+        var product = new ProductEntity("Test", "test desc", 15.5);
+
+        //Act
+        var storeProduct = productRepository.save(product);
+        this.storedProduct = storeProduct;
+
+        //Assert
+        assertEquals(product.getName(), storeProduct.getName());
+        assertEquals(product.getDesc(), storeProduct.getDesc());
+        assertEquals(product.getPrice(), storeProduct.getPrice());
+        assertNotNull(storeProduct.getId());
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Find by Product id and test with stored Product")
+    void createProduct_whenValidObject_returnsFindById() {
+        //Arrange
+
+        //Act
+        var storedProduct = productRepository.findById(this.storedProduct.getId()).orElseThrow();
+
+        //Assert
+        assertNotNull(storedProduct.getName());
+        assertNotNull(storedProduct.getDesc());
+        assertNotNull(storedProduct.getPrice());
+        assertNotNull(storedProduct.getId());
+
+        assertEquals(storedProduct.getId(), this.storedProduct.getId());
+        assertEquals(storedProduct.getName(), this.storedProduct.getName());
+        assertEquals(storedProduct.getDesc(), this.storedProduct.getDesc());
+        assertEquals(storedProduct.getPrice(), this.storedProduct.getPrice());
     }
 
 }

638ce91dab3984c08d8c4053aea4b26617b50f27
Pedro Salomon <pedro.salomon@gmail.com>
2026-01-13 15:20:44 +0100

    testing cascade persist and remove managed entity

--- backend/project-09/src/test/java/com/jsharper/dyndns/server/repositories/cascade/CustomerCascadeRepositoryTest.java
+++ backend/project-09/src/test/java/com/jsharper/dyndns/server/repositories/cascade/CustomerCascadeRepositoryTest.java
@@ -5,9 +5,12 @@ import com.jsharper.dyndns.server.entities.cascade.PhoneCascade;
 import com.jsharper.dyndns.server.repository.cascade.CustomerCascadeRepository;
 import com.jsharper.dyndns.server.repository.cascade.PhoneCascadeRepository;
 import jakarta.persistence.Tuple;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.util.Pair;
 import org.springframework.data.util.StreamUtils;
 import org.springframework.test.context.ActiveProfiles;
@@ -149,6 +152,43 @@ public class CustomerCascadeRepositoryTest {
                         }
                 )
         );
+
+    }
+
+    @Test
+    @Order(3)
+    void createCustomerWithChildrenAndChildrenSavedParentCascadePersistAndRemoveManagedEntity_providedCustomerAndChildrenPhoneCascade_returnThrowsExceptionConstraintViolation() {
+        var phones = new HashSet<PhoneCascade>();
+
+
+        PhoneCascade phoneCascade = new PhoneCascade("12225566", "mobile");
+
+        phones.add(phoneCascade);
+
+        PhoneCascade phoneCascade1 = new PhoneCascade("0585846565", "mobile");
+
+        phones.add(phoneCascade1);
+
+        PhoneCascade phoneCascade2 = new PhoneCascade("101010101", "pickup");
+
+        phones.add(phoneCascade2);
+
+        var customer = new CustomerCascade("Test Name", phones);
+
+        phoneCascade.setCustomerCascade(customer);
+
+        phoneCascade1.setCustomerCascade(customer);
+
+        phoneCascade2.setCustomerCascade(customer);
+
+        var storedCustomer = cr.save(customer);
+
+        Assertions.assertEquals(storedCustomer, customer);
+
+        var exception = Assertions.assertThrows(DataIntegrityViolationException.class, () -> cr.deleteById(storedCustomer.getId()));
+
+        MatcherAssert.assertThat(exception.getMessage(), CoreMatchers.containsString("could not execute statement [Referential integrity constraint violation:"));
+
 
     }
 }

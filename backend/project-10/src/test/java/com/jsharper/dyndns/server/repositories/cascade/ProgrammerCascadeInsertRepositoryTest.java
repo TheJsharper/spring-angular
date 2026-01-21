a7ce9019ad72784de81d0e06203f46172feda9f8
Pedro Salomon <pedro.salomon@gmail.com>
2026-01-21 20:18:15 +0100

    Cascade.persist birng to limit Exception With detached Entity

--- backend/project-10/src/test/java/com/jsharper/dyndns/server/repositories/cascade/ProgrammerCascadeInsertRepositoryTest.java
+++ backend/project-10/src/test/java/com/jsharper/dyndns/server/repositories/cascade/ProgrammerCascadeInsertRepositoryTest.java
@@ -2,9 +2,13 @@ package com.jsharper.dyndns.server.repositories.cascade;
 
 import com.jsharper.dyndns.server.entities.cascade.ProgrammerCascadeInsert;
 import com.jsharper.dyndns.server.entities.cascade.ProjectCascadeInsert;
+import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.PersistenceUnit;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matcher;
+import org.hamcrest.MatcherAssert;
 import org.hibernate.engine.spi.SessionImplementor;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -87,6 +91,49 @@ public class ProgrammerCascadeInsertRepositoryTest {
         transaction.commit();
 
         Assertions.assertEquals(storedProgrammer, programmer);
+        Assertions.assertEquals(1, storedProgrammer.getProjectCascadeInserts().size());
+
+    }
+
+
+    @Test
+    @Order(3)
+    void createAndTryPersistWithEntityManagerPropagatedFromParentToChildren_providedParentObjectAndListOfChildren_returnException() {
+
+        SessionImplementor session = (SessionImplementor) entityManager.getDelegate();
+
+        var transaction = session.getTransaction();
+
+        transaction.begin();
+
+        var failureMessage = "Detached entity passed to persist:";
+
+        var projects = new HashSet<ProjectCascadeInsert>();
+
+        projects.add(new ProjectCascadeInsert("SpringFramework Project"));
+
+        var programmer = new ProgrammerCascadeInsert("firstName", "lastName", projects);
+
+        session.persist(programmer);
+
+
+        var storedProgrammer = entityManager.find(ProgrammerCascadeInsert.class, programmer.getId());
+
+        session.detach(projects.toArray()[0]);
+
+        var exception = Assertions.assertThrows(EntityExistsException.class, () -> {
+            session.persist(programmer);
+            session.flush();
+            session.clear();
+            transaction.rollback();
+        });
+
+        Matcher<String> matcher = CoreMatchers.containsString(failureMessage);
+
+        MatcherAssert.assertThat(exception.getMessage(), matcher);
+
+        Assertions.assertEquals(storedProgrammer, programmer);
+
         Assertions.assertEquals(1, storedProgrammer.getProjectCascadeInserts().size());
 
     }

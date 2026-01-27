ec2f0e30bce83f07b6f23fae9a96216578b14786
Pedro Salomon <pedro.salomon@gmail.com>
2026-01-27 15:12:05 +0100

    component singleton for products

--- frontend/project-02/apps/web-app/src/app/app.routes.ts
+++ frontend/project-02/apps/web-app/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Route } from '@angular/router';
+import {ProductsService} from '@services';
 
 export const appRoutes: Route[] = [
     {
         path: '',
-        loadComponent: () => import('products').then(c => c.Products)
+        loadComponent: () => import('products').then(c => c.Products),
+        providers:[ProductsService]
     }
 ];

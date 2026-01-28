1a8dff8e1231dea82b7c2375c2d3fb5f5133486c
Pedro Salomon <pedro.salomon@gmail.com>
2026-01-28 19:57:37 +0100

    basics navigation

--- frontend/project-02/apps/web-app/src/app/app.routes.ts
+++ frontend/project-02/apps/web-app/src/app/app.routes.ts
@@ -4,7 +4,9 @@ import {ProductsService} from '@services';
 export const appRoutes: Route[] = [
     {
         path: '',
-        loadComponent: () => import('products').then(c => c.Products),
+       // loadComponent: () => import('products').then(c => c.Products),
+        loadComponent: () => import('main-shell').then(c => c.MainShell),
+    
         providers:[ProductsService]
     }
 ];
---
+++ frontend/project-02/libs/main-shell/src/lib/lib.routes.ts
@@ -0,0 +1,4 @@
+import { Route } from '@angular/router';
+import { MainShell } from './main-shell/main-shell';
+
+export const mainShellRoutes: Route[] = [{ path: '', component: MainShell }];

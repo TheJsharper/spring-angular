69c923f85f69560867d7cf45116aa96f88c7b82a
Pedro Salomon <pedro.salomon@gmail.com>
2026-01-31 14:10:30 +0100

    web-spa added

--- frontend/project-02/apps/web-spa/src/app/app.routes.ts
+++ frontend/project-02/apps/web-spa/src/app/app.routes.ts
@@ -6,8 +6,8 @@ export const appRoutes: Route[] = [
     { path: 'account', loadComponent: () => import('./components/account/account.component').then(m => m.AccountComponent) },
     //{ path: 'home', component: HomeComponent },
     { path: 'home', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
-   // { path: 'settings', component: SettingsComponent },
+    // { path: 'settings', component: SettingsComponent },
     { path: 'settings', loadComponent: () => import('./components/settings/settings.component').then(m => m.SettingsComponent) },
-    {path:'products', loadChildren:()=> import('products').then(m=>m.productsRoutes), providers: [ProductsService]},
+    { path: 'products', loadChildren: () => import('products').then(m => m.productsRoutes), providers: [ProductsService] },
     { path: '**', redirectTo: '/home' }
 ];

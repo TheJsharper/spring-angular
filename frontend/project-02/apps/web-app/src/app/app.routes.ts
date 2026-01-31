2cb37f1aebceb65c72d5dc2a12b1e34c087f6879
Pedro Salomon <pedro.salomon@gmail.com>
2026-01-31 14:59:18 +0100

    fixed spa

--- frontend/project-02/apps/web-app/src/app/app.routes.ts
+++ frontend/project-02/apps/web-app/src/app/app.routes.ts
@@ -3,18 +3,28 @@ import { ProductsService } from '@services';
 import { MainShell, mainShellRoutes } from 'main-shell';
 
 export const appRoutes: Route[] = [
-    {
+   /* {
         path: 'main-shell',
         // loadComponent: () => import('products').then(c => c.Products),
-        loadChildren: () => import('main-shell').then(c => c.mainShellRoutes),
-       // component: MainShell,
+         loadChildren: () => import('main-shell').then(c => c.mainShellRoutes),
+       // loadComponent: () => import('main-shell').then(c => c.MainShell),
 
-        providers: [ProductsService,  ],
+        providers: [ProductsService],
 
     },
     {
         path: '',
         pathMatch: 'full',
         redirectTo: '/main-shell',
-    }
+    }*/
+
+       
+    {
+        path: 'products',
+        //loadComponent: () => import('products').then(m => m.Products),
+         loadChildren: () => import('products').then(m => m.productsRoutes),
+         providers: [ProductsService],
+       // component: Products
+
+    },
 ];
--- frontend/project-02/apps/web-app/src/app/app.ts
+++ frontend/project-02/apps/web-app/src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  imports: [ RouterModule],
+  imports: [RouterModule, MatListModule, MatSidenavModule, MatIconModule, MatButtonModule, MatToolbarModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected title = 'web-app';
+    protected readonly isMobile = signal(true);
 }

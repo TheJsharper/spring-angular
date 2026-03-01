2083d5f6b02f1af5ab240a88ccd60654c0713040
Pedro Salomon <pedro.salomon@gmail.com>
2026-03-01 11:19:43 +0100

    chart libraries added

---
+++ frontend/project-02/libs/charts/line-chart/src/lib/line-chart/line-chart.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-line-chart',
+  imports: [],
+  templateUrl: './line-chart.html',
+  styleUrl: './line-chart.scss',
+})
+export class LineChart {}

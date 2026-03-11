b873e1b713b33f67393c919179ab69b5ace559f6
Pedro Salomon <pedro.salomon@gmail.com>
2026-03-11 20:20:12 +0100

    sunburst and TreeMap swithching option

--- frontend/project-02/libs/charts/treemap-chart/src/lib/treemap-chart/treemap-chart.ts
+++ frontend/project-02/libs/charts/treemap-chart/src/lib/treemap-chart/treemap-chart.ts
@@ -1,9 +1,10 @@
-import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { ActivatedRoute, Router } from '@angular/router';
-import { TreeChartService, TreemapChartService } from '@services';
+import { TreemapChartService } from '@services';
 import * as echarts from 'echarts';
-import { Subscription } from 'rxjs';
+import { EChartsPackageSize } from 'libs/services/src/lib/types/echarts-package-size';
+import { map, mergeMap, Subscription, timer } from 'rxjs';
 
 @Component({
   selector: 'lib-treemap-chart',
@@ -27,14 +28,23 @@ export class TreemapChart implements OnInit, OnDestroy {
 
     this.chartInstance = echarts.init(chartDom);
 
-    this.subscription.add(
-      this.treemapChartService.getData().subscribe(data => {
-        if (this.chartInstance) {
-          const option = this.treemapChartService.getOptionTreemap(data);
-          this.chartInstance.setOption(option);
-        }
+    this.subscription.add(this.treemapChartService.getData().pipe(
+      mergeMap((data: EChartsPackageSize) => {
+        const sunburstOption = this.treemapChartService.getOptionSunburst(data);
+
+        const treepMapOption = this.treemapChartService.getOptionTreemap(data);
+        
+        return timer(0, 5000).pipe(map((tic) => {
+          if (this.chartInstance) {
+            if (tic % 2 === 0) {
+              this.chartInstance.setOption(treepMapOption);
+            } else {
+              this.chartInstance.setOption(sunburstOption);
+            }
+          }
+        }));
       })
-    );
+    ).subscribe());
   }
 
   ngOnDestroy(): void {

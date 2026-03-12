711b26662e34d0168b28ab053d7cba351ca83371
Pedro Salomon <pedro.salomon@gmail.com>
2026-03-12 22:57:22 +0100

    score up and down added

--- frontend/project-02/libs/charts/gauge-charts/src/lib/gauge-charts/simple-gauge/simple-gauge.component.ts
+++ frontend/project-02/libs/charts/gauge-charts/src/lib/gauge-charts/simple-gauge/simple-gauge.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
-import { FunnelChartService, GaugeChartService } from '@services';
+import { MatButtonModule } from "@angular/material/button";
+import { GaugeChartService } from '@services';
 import * as echarts from 'echarts';
 @Component({
   selector: 'app-simple-gauge',
+  imports: [MatButtonModule],
   templateUrl: './simple-gauge.component.html',
   styleUrls: ['./simple-gauge.component.scss']
 })
@@ -16,6 +18,38 @@ export class SimpleGaugeComponent implements OnInit, OnDestroy {
     const options = this.gaugeChartService.getSimpleGaugeOptions();
     this.chartInstance = echarts.init(document.getElementById('gauge-chart-simple') as HTMLElement);
     this.chartInstance.setOption(options);
+  }
+
+  upScore(): void {
+    if (this.chartInstance) {
+      const option = this.chartInstance.getOption() as {
+        series?: Array<{ data?: Array<{ value?: number }> }>;
+      };
+      const currentValue = Number(option.series?.[0]?.data?.[0]?.value ?? 0);
+      const newValue = Math.min(currentValue + 1, 100);
+
+      this.chartInstance.setOption({
+        series: [{
+          data: [{ value: newValue, name: 'SCORE' }]
+        }]
+      });
+    }
+  }
+
+  downScore(): void {
+    if (this.chartInstance) {
+      const option = this.chartInstance.getOption() as {
+        series?: Array<{ data?: Array<{ value?: number }> }>;
+      };
+      const currentValue = Number(option.series?.[0]?.data?.[0]?.value ?? 0);
+      const newValue = Math.max(currentValue - 1, 0);
+
+      this.chartInstance.setOption({
+        series: [{
+          data: [{ value: newValue, name: 'SCORE' }]
+        }]
+      });
+    }
   }
 
   ngOnDestroy(): void {

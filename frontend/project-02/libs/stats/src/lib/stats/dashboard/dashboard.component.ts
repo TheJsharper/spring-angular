a5b0ff8b756135338fcbe4f8444962373e7b0f3e
Pedro Salomon <pedro.salomon@gmail.com>
2026-03-24 22:22:43 +0100

    fixing directive problems

--- frontend/project-02/libs/stats/src/lib/stats/dashboard/dashboard.component.ts
+++ frontend/project-02/libs/stats/src/lib/stats/dashboard/dashboard.component.ts
@@ -1,8 +1,9 @@
-import { AsyncPipe, NgComponentOutlet } from "@angular/common";
+import { AsyncPipe } from "@angular/common";
 import { AfterContentInit, ChangeDetectionStrategy, Component, inject } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { ActivatedRoute, Router } from "@angular/router";
+import { Observable } from "rxjs";
 import { StatsDashboardItem } from "../../models/stats-dashboard.model";
 import { StatsIconItem } from "../../models/stats-icons.model";
 import { StatsService } from "../../services/stats.service";
@@ -14,31 +15,29 @@ import { ActionStatsDirective } from "../card-stats/directives/action-stats.dire
     selector: 'lib-dashboard',
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.scss'],
-    imports: [MatButtonModule, MatCardModule, CardStatsComponent, CardStatsDirective, ActionStatsDirective],
+    imports: [MatButtonModule, MatCardModule, CardStatsComponent, CardStatsDirective, ActionStatsDirective, AsyncPipe],
     changeDetection: ChangeDetectionStrategy.OnPush
-    
+
 })
 export class DashboardComponent implements AfterContentInit {
-    
+
     router: Router = inject(Router);
 
     route: ActivatedRoute = inject(ActivatedRoute);
-    
+
     statsService: StatsService = inject(StatsService);
-    
-    data: (StatsDashboardItem & StatsIconItem )[] = [];
+
+    data$: Observable<(StatsDashboardItem & StatsIconItem)[]> = this.statsService.getData();
+
 
     ngAfterContentInit(): void {
-        this.statsService.getData().subscribe((data) => {
-            console.log(data);
-            this.data = data;
-        });
+        this.data$ = this.statsService.getData();
     }
 
     trackBy(index: number, dashboardItem: StatsDashboardItem & StatsIconItem): any {
         return dashboardItem.name; // Assuming 'name' is a unique identifier for each dashboard item
     }
-    
+
     nav(goTo: string): void {
         this.router.navigate([`/stats/${goTo}`], { relativeTo: this.route });
     }
